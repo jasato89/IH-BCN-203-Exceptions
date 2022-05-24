1cea6442dff11c82198052225e46ad8b2d058884
jasato89 <jasato89@gmail.com>
2022-05-24 11:56:38 +0200

    throws

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
+         /*
         //Checked Exceptions - Unchecked Exceptions
 
         //Checked Exception
@@ -41,7 +44,26 @@ public class Main {
         System.out.println("==========");
 
         printArrayManagingExceptions(numbers2);
-        
+
+        System.out.println(convertToInt("16"));
+        System.out.println(convertToInt("Hola"));
+
+       int i;
+
+        try {
+            i = convertToIntWithThrows("Hola");
+
+        } catch (Exception e) {
+            System.out.println("Error");
+            i = 12;
+        }
+
+        try {
+            FileWriter writer = new FileWriter("hola.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
@@ -66,7 +88,6 @@ public class Main {
 
         } catch (NullPointerException e) {
 
-            e.printStackTrace();
         } finally {
             System.out.println("Tarea terminada");
         }
@@ -74,7 +95,20 @@ public class Main {
     }
 
     public static int convertToInt(String number) {
+        int result = 0;
+        try {
+            result = Integer.parseInt(number);
+        } catch (NumberFormatException numberFormatException) {
+            System.err.println("Formato del string de entrada no válido");
+        } catch (NullPointerException e) {
+            System.out.println("Objeto no existe");
+        }
 
+        return result;
+    }
+
+    public static int convertToIntWithThrows(String number) throws Exception {
+        return Integer.parseInt(number);
     }
 
 }
